c1c9102e545a1b4876d92ff16888ce03acc7a4da
Hazbie Dalipaj <hazbie.dalipaj@marinetraffic.com>
2020-02-03 12:52:00 +0200

    rate the app

--- src/component/rating/rating.jsx
+++ src/component/rating/rating.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 import Box from '@material-ui/core/Box';
+import './rating.css'
 
 const labels = {
   1: 'Useless',
@@ -24,23 +25,32 @@ const useStyles = makeStyles({
   },
 });
 
+
 const HoverRating = () => {
-  const [rate, setRate] = useState(labels);
+  const [rate, setRate] = useState(null);
   const [hover, setHover] = useState(-1);
   const classes = useStyles();
 
   console.log(`You rate is ${rate}`)
 
   return (
+    <>
       <div className = 'tc'>
         <ol className='heading'>Please rate the app</ol>     
         <div className={classes.rating}>        
         <Rating name="hover-feedback" value={rate} size="large" max={10} onChange={(event, newRate) => { setRate(newRate); }}
         onChangeActive={(event, newHover) => { setHover(newHover); }}
       />
-      {rate !== null && <Box ml={4}>{labels[hover !== -1 ? hover : rate]}</Box>}
+      {rate !== null && <Box ml={4}>{labels[hover !== -1 ? hover : rate]}</Box>}      
         </div>
     </div>
+    <div className='message'>
+      You just rate with: {rate}<br></br>
+      Thank You!!
+    </div>
+      
+    </>
+      
   );
 }
 export default HoverRating;
